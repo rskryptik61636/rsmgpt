4eda51746b423065dff4609dc93826a319406fb3
Rohit Shrinath <mk.rohit.shrinath@gmail.com>
2015-10-03 23:51:01 -0700

    Startup script.

---
+++ rsmgpt.cmd
@@ -0,0 +1,9 @@
+rem startup script for rsmgpt
+
+rem setup the environment variables
+set RSMGPT_ROOT=N:\rsmgpt\
+set DXTK_ROOT=N:\DirectXTK\
+set VS_EXE="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"
+
+rem start visual studio
+start /D %RSMGPT_ROOT% /B %VS_EXE% %RSMGPT_ROOT%rsmgpt.sln
