78f5373f6e476f0c23943af4dd1813450f618e00
Rohit Shrinath <mk.rohit.shrinath@gmail.com>
2015-10-03 23:50:49 -0700

    Started adding resource and state initialization code.

--- src/rsmgptEngine.cpp
+++ src/rsmgptEngine.cpp
@@ -27,7 +27,8 @@ namespace rsmgpt
 {
 	// Engine ctor.
 	Engine::Engine(const path sceneFile)
-		: DXSample(800, 600, L"rsmgpt")	// TODO: Need a way to read the width and height from the scene file.
+		: DXSample(800, 600, L"rsmgpt"),	// TODO: Need a way to read the width and height from the scene file.
+        m_frameIndex(0)
 	{
 		// TODO: Add implementation here.
 	}
@@ -40,6 +41,109 @@ namespace rsmgpt
 
 	void Engine::OnInit()
 	{
+        // Enable the D3D12 debug layer if in debug mode.
+        #ifdef _DEBUG
+            {
+                ComPtr<ID3D12Debug> debugController;
+                if( SUCCEEDED( D3D12GetDebugInterface( IID_PPV_ARGS( &debugController ) ) ) )
+                {
+                    debugController->EnableDebugLayer();
+                }
+            }
+        #endif
+
+        // Create a DXGIFactory and D3D12Device.
+        ComPtr<IDXGIFactory4> factory;
+        ThrowIfFailed( CreateDXGIFactory1( IID_PPV_ARGS( &factory ) ) );
+
+        ThrowIfFailed( 
+            D3D12CreateDevice(
+                nullptr,                    // Video adapter. nullptr implies the default adapter.
+                D3D_FEATURE_LEVEL_11_0,     // D3D feature level.
+                IID_PPV_ARGS( &m_device )   // D3D device object.
+                ) );
+
+        // Create the render and compute command queues that we will be using.
+        D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+        queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        ThrowIfFailed( m_device->CreateCommandQueue( &queueDesc, IID_PPV_ARGS( &m_commandQueue ) ) );
+
+        D3D12_COMMAND_QUEUE_DESC computeQueueDesc = {};
+        computeQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        computeQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_COMPUTE;
+        ThrowIfFailed( m_device->CreateCommandQueue( &computeQueueDesc, IID_PPV_ARGS( &m_computeCommandQueue ) ) );
+
+        // Describe and create the swap chain.
+        DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+        swapChainDesc.BufferCount = FrameCount;
+        swapChainDesc.BufferDesc.Width = m_width;
+        swapChainDesc.BufferDesc.Height = m_height;
+        swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+        swapChainDesc.OutputWindow = m_hwnd;
+        swapChainDesc.SampleDesc.Count = 1;
+        swapChainDesc.Windowed = TRUE;
+
+        ComPtr<IDXGISwapChain> swapChain;
+        ThrowIfFailed( factory->CreateSwapChain(
+            m_commandQueue.Get(),		// Swap chain needs the render queue so that it can force a flush on it.
+            &swapChainDesc,
+            &swapChain
+            ) );
+        ThrowIfFailed( swapChain.As( &m_swapChain ) );
+
+        // Set m_frameIndex to the current back buffer index.
+        m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+        // Create descriptor heaps.
+        {
+            // Describe and create a render target view (RTV) descriptor heap.
+            D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+            rtvHeapDesc.NumDescriptors = FrameCount;
+            rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+            rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+            ThrowIfFailed( m_device->CreateDescriptorHeap( &rtvHeapDesc, IID_PPV_ARGS( &m_rtvHeap ) ) );
+
+#if 0
+            // Describe and create a depth stencil view (DSV) descriptor heap.
+            D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc = {};
+            dsvHeapDesc.NumDescriptors = 1;
+            dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+            dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+            ThrowIfFailed( m_device->CreateDescriptorHeap( &dsvHeapDesc, IID_PPV_ARGS( &m_dsvHeap ) ) );
+#endif // 0
+
+
+            // Describe and create a constant buffer view (CBV), Shader resource
+            // view (SRV), and unordered access view (UAV) descriptor heap.
+            D3D12_DESCRIPTOR_HEAP_DESC cbvSrvUavHeapDesc = {};
+            cbvSrvUavHeapDesc.NumDescriptors = CbvSrvUavDescriptorCountPerFrame * FrameCount;
+            cbvSrvUavHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+            cbvSrvUavHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+            ThrowIfFailed( m_device->CreateDescriptorHeap( &cbvSrvUavHeapDesc, IID_PPV_ARGS( &m_cbvSrvUavHeap ) ) );
+
+            m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize( D3D12_DESCRIPTOR_HEAP_TYPE_RTV );
+            m_cbvSrvUavDescriptorSize = m_device->GetDescriptorHandleIncrementSize( D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV );
+        }
+
+        // Create frame resources.
+        {
+            CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle( m_rtvHeap->GetCPUDescriptorHandleForHeapStart() );
+
+            // Create a RTV and command allocators for each frame.
+            for( UINT n = 0; n < FrameCount; n++ )
+            {
+                ThrowIfFailed( m_swapChain->GetBuffer( n, IID_PPV_ARGS( &m_renderTargets[ n ] ) ) );
+                m_device->CreateRenderTargetView( m_renderTargets[ n ].Get(), nullptr, rtvHandle );
+                rtvHandle.Offset( 1, m_rtvDescriptorSize );
+
+                ThrowIfFailed( m_device->CreateCommandAllocator( D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS( &m_commandAllocators[ n ] ) ) );
+                ThrowIfFailed( m_device->CreateCommandAllocator( D3D12_COMMAND_LIST_TYPE_COMPUTE, IID_PPV_ARGS( &m_computeCommandAllocators[ n ] ) ) );
+            }
+        }
+            
 		// TODO: Add implementation here.
 	}
 
--- src/rsmgptEngine.h
+++ src/rsmgptEngine.h
@@ -23,6 +23,7 @@
 #pragma once
 
 #include <DXSample.h>
+#include "rsmgptDefns.h"
 
 #include <filesystem>
 
@@ -43,6 +44,101 @@ namespace rsmgpt
 		virtual void OnRender() override;
 		virtual void OnDestroy() override;
 		virtual bool OnEvent(MSG msg) override;
+
+        // Engine constants.
+        static const UINT FrameCount = 3;   // TODO: Rename to m_FrameCount once we've got whatever code we need from the D3D12 samples.
+
+        // D3D pipeline objects
+        ComPtr<ID3D12Device> m_device;
+        ComPtr<IDXGISwapChain3> m_swapChain;
+        ComPtr<ID3D12Resource> m_renderTargets[ FrameCount ];
+        ComPtr<ID3D12CommandAllocator> m_commandAllocators[ FrameCount ];
+        ComPtr<ID3D12CommandAllocator> m_computeCommandAllocators[ FrameCount ];
+        ComPtr<ID3D12CommandQueue> m_commandQueue;  // TODO: Rename to m_renderCommandQueue once we've got all the code we need from the D3D12 samples.
+        ComPtr<ID3D12CommandQueue> m_computeCommandQueue;
+        ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+        //ComPtr<ID3D12DescriptorHeap> m_dsvHeap;   // NOTE: May not need right now, leaving behind just in case.
+        ComPtr<ID3D12DescriptorHeap> m_cbvSrvUavHeap;
+
+        // Engine variables.
+        UINT m_frameIndex;
+        UINT m_rtvDescriptorSize;
+        UINT m_cbvSrvUavDescriptorSize;
+
+        // Synchronization objects.
+        ComPtr<ID3D12Fence> m_fence;
+        ComPtr<ID3D12Fence> m_computeFence;
+        UINT64 m_fenceValues[ FrameCount ];
+        HANDLE m_fenceEvent;
+
+        // Asset objects.
+        ComPtr<ID3D12PipelineState> m_pipelineState;    // TODO: Rename to m_renderPipelineState once we've got all the code we need from the D3D12 samples.
+        ComPtr<ID3D12PipelineState> m_computePipelineState;
+        ComPtr<ID3D12GraphicsCommandList> m_commandList;    // TODO: Rename to m_renderCommandList once we've got all the code we need from the D3D12 samples.
+        ComPtr<ID3D12GraphicsCommandList> m_computeCommandList;
+        ComPtr<ID3D12Resource> m_cbPerFrameResource;
+        ComPtr<ID3D12Resource> m_commandBuffer;
+        //ComPtr<ID3D12Resource> m_depthStencil;
+        //ComPtr<ID3D12Resource> m_vertexBuffer;
+        /*ComPtr<ID3D12Resource> m_processedCommandBuffers[ m_frameCount ];
+        ComPtr<ID3D12Resource> m_processedCommandBufferCounters[ m_frameCount ];
+        UINT8* m_pMappedUavCounters[ m_frameCount ];
+        D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;*/
+
+        // Constant buffers.
+
+        // Per frame constants.
+        // TODO: float[] will need to be swapped out for the XMFloat type.
+        struct cbPerFrame
+        {
+            Vec3    gCamPos;            // Camera position.
+            float   gCamAspectRatio;    // Camera aspect ratio.
+            Vec3    gCamDir;            // Camera direction.
+
+            float   pad2[ 57 ];         // Constant buffers are 256 byte aligned.
+        } m_cbPerFrame;
+        //cbPerFrame m_cbPerFrame;
+
+        // Root constants for the compute shader.
+        struct CSRootConstants
+        {
+            float xOffset;
+            float zOffset;
+            float cullOffset;
+            float commandCount;
+        };
+
+        // Data structure to match the command signature used for ExecuteIndirect.
+        struct IndirectCommand
+        {
+            D3D12_GPU_VIRTUAL_ADDRESS cbv;
+            D3D12_DRAW_ARGUMENTS drawArguments;
+        };
+
+        // Graphics root signature parameter offsets.
+        enum GraphicsRootParameters
+        {
+            Cbv,
+            GraphicsRootParametersCount
+        };
+
+        // Compute root signature parameter offsets.
+        enum ComputeRootParameters
+        {
+            SrvUavTable,
+            RootConstants,			// Root constants that give the shader information about the triangle vertices and culling planes.
+            ComputeRootParametersCount
+        };
+
+        // CBV/SRV/UAV desciptor heap offsets.
+        enum HeapOffsets
+        {
+            CbvOffset = 0,
+            CbvSrvOffset = CbvOffset /*+ TriangleCount*/,							// SRV that points to the constant buffers used by the rendering thread.
+            CommandsOffset = CbvSrvOffset + 1,									// SRV that points to all of the indirect commands.
+            ProcessedCommandsOffset = CommandsOffset + 1,						// UAV that records the commands we actually want to execute.
+            CbvSrvUavDescriptorCountPerFrame = ProcessedCommandsOffset + 1		// 1 CBV per triangle + [2 SRVs + 1 UAV for the compute shader].
+        };
 	};
 
 }	// end of namespace rsmgpt
